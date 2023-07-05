12818e9f79164f5cf33e27894977e214f19b7f63
Luna <luni.siaa@gmail.com>
2023-07-05 15:33:43 +0200

    return all files on `/v0`

--- src/api/v0/get.ts
+++ src/api/v0/get.ts
@@ -1,7 +1,15 @@
+import path from "path";
+
+import walkAllDirs from "../../module/walkAllDirs";
 import { RouteRun } from "../../typings";
 
-const run: RouteRun<string> = async () => {
-    return "👋";
+const run: RouteRun<string[]> = async (request, reply) => {
+
+    const filePath = path.join(process.cwd(), "static");
+    const files = await walkAllDirs(filePath, ".json");
+
+    reply.header("Content-Type", "application/json");
+    return files.map((file) => file.split(filePath)[1]);
 };
 
 export default { run };
