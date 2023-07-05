eb528e84b290d867cd38c19a0bf804d6a2c1a4eb
Luna <luni.siaa@gmail.com>
2023-07-05 15:32:58 +0200

    walkAllDirs delete cache indev, add filetype

--- src/module/walkAllDirs.ts
+++ src/module/walkAllDirs.ts
@@ -1,12 +1,20 @@
 import { readdir, stat } from "fs/promises";
 import path from "path";
 
-export default async function walkAllDirs(dir: string): Promise<string[]> {
+import { Config } from "../store";
+
+export default async function walkAllDirs(dir: string, fileType?: `.${string}` | undefined): Promise<string[]> {
     const results: string[] = [];
     for await (const dirItem of await readdir(dir)) {
-        const fstat = await stat(path.join(dir, dirItem));
-        if (fstat.isFile() && dirItem.endsWith(".js")) results.push(path.join(dir, dirItem));
-        if (fstat.isDirectory()) (await walkAllDirs(path.join(dir, dirItem))).forEach((walkItem) => results.push(walkItem));
+        const fileName = path.join(dir, dirItem);
+        const fstat = await stat(fileName);
+
+        if (fstat.isFile()) {
+            if (Config.dev) delete require.cache[require.resolve(fileName)];
+            if (dirItem.endsWith(fileType || ".js")) results.push(fileName);
+        }
+
+        if (fstat.isDirectory()) await (await walkAllDirs(fileName, fileType)).forEach((walkItem) => results.push(walkItem));
     }
     return results;
 }
