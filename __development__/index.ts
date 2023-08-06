2deb24e858e14f40d3832a53a00607757aac9cca
Luna <luni.siaa@gmail.com>
2023-08-06 21:46:07 +0200

    Delete __development__ directory

--- __development__/index.ts
+++
@@ -1,4 +0,0 @@
-import webserver from "./module/load/webserver";
-import { Config } from "./store";
-
-webserver.load().then(() => webserver.start(Config.api.port));
--- __development__/typings.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-
-export interface Route { run: RouteRun<unknown> }
-export type RouteRun<T> = (request: FastifyRequest, reply: FastifyReply) => Promise<T | RouteErrorResponse | undefined>;
-export interface RouteErrorResponse {
-    statusCode: number;
-    message: string;
-}
